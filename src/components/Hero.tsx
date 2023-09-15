842e0634ff915a312f1ab2df03653303b3fc6aab
Adham <32067614+azakiio@users.noreply.github.com>
2023-09-15 16:57:45 +0200

    updated formatting

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,19 +1,17 @@
 import Image, { StaticImageData } from "next/image";
-Image
+
+
 export default function Hero({ title, content, image }: Props) {
+  
   return (
     <div className="gap-4 border">
-
-
       <h1 className="fw-bold text-4xl text-center mb-12">{title}</h1>
-
 
       <div className="flex gap-4">
         <div>{content}</div>
-        <Image src={image}  alt="" className="rounded-t-full" />
+        <Image src={image} alt="" className="rounded-t-full" />
         <img src="https://picsum.photos/200/300" />
       </div>
-
     </div>
   );
 }
